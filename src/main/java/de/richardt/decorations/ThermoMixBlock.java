5815f8bec906735660659bb483916cf2dd353fab
Daniel Richardt <D.Richardt@t-systems.com>
2023-12-06 08:57:39 +0100

    Cleanup unused imports

--- src/main/java/de/richardt/decorations/ThermoMixBlock.java
+++ src/main/java/de/richardt/decorations/ThermoMixBlock.java
@@ -5,21 +5,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 
 
 public class ThermoMixBlock extends HorizontalFacingBlock {
